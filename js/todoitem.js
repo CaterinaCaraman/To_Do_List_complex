1a92556fbb21ea0f77358720c031d8b72edf57dc
CaterinaCaraman <cecan.ecaterina@gmail.com>
2020-12-15 21:29:05 +0200

    First commit

---
+++ js/todoitem.js
@@ -0,0 +1,20 @@
+export default class ToDoItem {
+  constructor() {
+    this._id = null;
+    this._item = null;
+  }
+
+  getId() {
+    return this._id;
+  }
+
+  setId(id) {
+    this._id = id;
+  
+  }
+
+  getItem() {
+    this._item = item;
+  }
+
+}
